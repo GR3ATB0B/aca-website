35fbeaa2faaa856cb75744d6785bda9e28c6b7cb
Nash Vogeltanz <nashvogeltanz@icloud.com>
2026-01-22 09:05:45 -0500

    cs stufffffffffy

--- CCode/master_commiter.c
+++ CCode/master_commiter.c
@@ -2,9 +2,21 @@
 #include <math.h>
  
 int main() {
-   double array[100];
+   float array[100];
+   float num;
    for(int i = 0; i <= 100; i++) { 
        array[i] = sqrt(i);
-         printf("%f\n", array[i]);
+        printf("%.2f", array[i]);
+         printf("\n");
     }
+
+    printf("enter the number you want to search for\n");
+    scanf("%f", &num);
+
+    for(int i = 0; i <= 100; i++) {
+        if(array[i] == num) {
+            printf("Number found at index %d\n", i);
+        }
+    }
+    printf("Number not found in the array\n");
 }
