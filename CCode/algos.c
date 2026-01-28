6eefcdb7de0ff0a8ca5ade2d6728dd90dc69e71d
Nash Vogeltanz <nashvogeltanz@icloud.com>
2026-01-28 15:14:12 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- CCode/algos.c
+++ CCode/algos.c
@@ -30,10 +30,43 @@ void fith() {
     }
     }
 }
+void sixth() {
+    int arr[85] = {0};
+}
+
+void seventh() {
+    int matrix[20][20];
+   
+    }
+void eighth() {
+    int arr[85];
+    for (int i = 0; i < 85; i++) {
+        arr[i] = i;
+    }
+}
+void ninth() {
+    int rows = 5, cols = 5;
+    int arr[rows][cols];
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = i * cols + j;
+            printf("(%d,%d)\n", i, j);
+        }
+    }
+}
+
+
+
+
 int main() {
     first();
     second();
     third();
     fourth();
     fith();
+    sixth();
+    seventh();
+    eighth();
+    ninth();
 }
