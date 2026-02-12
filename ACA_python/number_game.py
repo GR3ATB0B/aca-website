d182986eb81178f3abc9d402c25e27e663457be3
Nash Vogeltanz <nashvogeltanz@icloud.com>
2026-02-12 15:06:41 -0500

    full num game

--- ACA_python/number_game.py
+++ ACA_python/number_game.py
@@ -2,4 +2,17 @@ import random
 
 num = random.randint(1, 100)
 
-print(num)
+print("Welcome to the Number Guessing Game!")
+print("I have selected a number between 1 and 100. Can you guess it?")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    
+    if guess < num:
+        print("Too low! Try again.")
+    elif guess > num:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You've guessed the number {num} correctly!")
+        break
+      
