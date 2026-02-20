8e2a90ee124019b23a5ad50b50cb7f9ac2113c04
Nash Vogeltanz <nashvogeltanz@gmail.com>
2026-02-20 12:14:20 -0500

    bruh

---
+++ ACA_python/Version_A.py
@@ -0,0 +1,35 @@
+#1
+city = "london"
+print(city)
+
+#2
+x = 12
+y =5
+print(x*y)
+print(x-y)
+
+#3
+print(x/y)
+print(x//y)
+
+#4
+base = 10
+hight = 5
+area = 0.5 * base * hight
+print(area)
+
+#5
+print(10 - 2 * 3)
+print((10 - 2) * 3)
+
+#6
+is_open = False
+print(is_open)
+
+#7 
+p = 15
+q = 20
+print(p > q)
+print(p != q)
+
+#
