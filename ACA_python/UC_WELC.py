b78f8c909dbcded6d3c9f843c7976f52c6105d6f
Nash Vogeltanz <nashvogeltanz@gmail.com>
2026-03-03 14:40:05 -0500

    UC_WELC.py

--- ACA_python/UC_WELC.py
+++ ACA_python/UC_WELC.py
@@ -1,5 +1,8 @@
-age = input("What is your age? ")
-name = input("What is your name? ") 
-major = input("What is your major? ")   
+def welcome():
+    age = input("What is your age? ")
+    name = input("What is your name? ") 
+    major = input("What is your major? ")
+    return age, name, major
 
+age, name, major = welcome()
 print (f"Hello {name}, you are {age} years old and your major is {major}. Welcome to UC!")
